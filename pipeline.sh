612e8bf987d37c52b6b459c977099b25f56b2707
andr / gohyun <64496558+Ayndrs@users.noreply.github.com>
2026-02-20 11:33:03 -0600

    remove files

--- pipeline.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "chunking data hit3.txt"
-cat hitch3.txt | python3 chonk.py
-ls part* | xargs -n 1 -P 4 cat | python3 mapper.py | sort | python3 reducer.py > out
-echo "Output generated in out"
-#less out
--- testing.py
+++
@@ -1,7 +0,0 @@
-from exmath import add, sub
-
-def test_add():
-    assert add(1, 1) == 2
-
-def test_sub():
-    assert sub(2, 1) == 1
